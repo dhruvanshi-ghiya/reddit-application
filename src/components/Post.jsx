ed5ff216746fec83f483a5f3cfb045aeeab2582c
dhru-conestoga <dghiya@conestogac.on.ca>
2024-03-14 21:58:25 -0400

    Styling

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment, faStar } from '@fortawesome/free-solid-svg-icons';
+import '../styles/Post.css';
 
 const Post = ({ post, onToggleFavorite }) => {
   const { id, score, title, commentsUrl, isFavorite } = post;
@@ -8,13 +11,27 @@ const Post = ({ post, onToggleFavorite }) => {
   };
 
   return (
-    <div>
-      <p>Score: {score}</p>
-      <p>Title: {title}</p>
-      <a href={commentsUrl}>Comments</a>
-      <button onClick={handleToggleFavorite}>
-        {isFavorite ? 'Remove from Favorites' : 'Add Favorite'}
-      </button>
+    <div className="post-card">
+      <div className="post-header">
+        <h3 className="post-title">{title}</h3>
+        <div className="post-score">
+          <FontAwesomeIcon icon={faStar} className="star-icon" />
+          <span>{score}</span>
+        </div>
+      </div>
+      <div className="post-body">
+        <a href={commentsUrl} className="comments-link">
+          <FontAwesomeIcon icon={faComment} className="comment-icon" />
+          Comments
+        </a>
+        <button onClick={handleToggleFavorite} className="favorite-btn">
+          <FontAwesomeIcon
+            icon={isFavorite ? faStar : ['far', 'star']}
+            className="favorite-icon"
+          />
+          {isFavorite ? 'Remove from Favorites' : 'Add Favorite'}
+        </button>
+      </div>
     </div>
   );
 };
